d0e95af23609105bbce4bf4ae11026f5633b7cae
Damian Rouson <damian@sourceryinstitute.org>
2018-11-07 11:42:56 -0800

    Add commented version of Day2 hands-on to solutio

--- src/Day-3/solution/test-asymmetric-load-check.f90
+++ src/Day-3/solution/test-asymmetric-load-check.f90
@@ -11,19 +11,24 @@ program main
 
    integer, allocatable :: nx(:)
    type(structured_grid) :: mesh
+     !! Encapsulate an allocatable array coarray component identical to the "x" coarray from
+     !! ../../Day-2/prepared-code/05-collective-load-check.f90
 
    nx = mesh%get_grid_resolution('grid-parameters.nml')
+     !! Refactor the get_grid_resolution function from ../../Day-2/prepared-code/05-collective-load-check.f90
    call assert( size(nx)==space_dimension .and. all(nx>0),"acceptable 3D grid resolution")
 
    call mesh%allocate_my_partition(nx)
+     !! Refactor the allocate_my_partition subroutine from ../../Day-2/prepared-code/05-collective-load-check.f90
+     !! to allow for a potentially uneven block distribution of y-z planes across images
 
    block
      integer load
      load = mesh%my_partition_size()
+       !! Refactor the my_partition_size function from ../../Day-2/prepared-code/05-collective-load-check.f90
      call co_sum( load )
      call assert( load == product(nx)*space_dimension, "all points distributed")
    end block
-
 
    sync all
    block
