1188240a9f20f2e2e608da861d605d20aeef6d5f
Damian Rouson <damian@sourceryinstitute.org>
2018-11-08 15:54:36 -0800

    Update Day-4/hands-on code

--- src/Day-4/hands-on/utilities/emulated_collectives_interface.f90
+++ src/Day-4/hands-on/utilities/emulated_collectives_interface.f90
@@ -2,18 +2,18 @@ module emulated_collectives_interface
   !! Fortran 2008 coarray emulations of Fortran 2018 intrinsic collective subroutines
   implicit none
 
-  interface co_sum
+  interface emulated_co_sum
     module procedure co_sum_integer
   end interface
 
-  interface co_broadcast
+  interface emulated_co_broadcast
     module procedure co_broadcast_integer
   end interface
 
   interface
 
     module subroutine co_sum_integer(a,result_image,stat,errmsg)
-      !! parallel computation of the sum of the first argument 
+      !! parallel computation of the sum of the first argument
       implicit none
       integer, intent(inout) :: a
       integer, intent(in), optional :: result_image
@@ -22,7 +22,7 @@ module emulated_collectives_interface
     end subroutine
 
     module subroutine co_broadcast_integer(a,source_image,stat,errmsg)
-      !! parallel one-to-all communication of the value of first argument 
+      !! parallel one-to-all communication of the value of first argument
       implicit none
       integer, intent(inout) :: a
       integer, intent(in) :: source_image
